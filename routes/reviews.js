63e56e601d47cda803d0e085d832096969726344
mikealberto <alberto.michael1701@gmail.com>
2022-10-18 15:48:33 -0400

    add route for posting review of a bread

--- routes/reviews.js
+++ routes/reviews.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const reviewsCtrl = require("../controllers/reviews");
 const isLoggedIn = require("../config/auth");
 
-router.get("/breads/:id/reviews", isLoggedIn, reviewsCtrl.create);
+router.post("/breads/:id/reviews", isLoggedIn, reviewsCtrl.create);
 
 module.exports = router;
 
