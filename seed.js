5886ad87a43ab6d24aa2ccc96f85fdba55daa3a1
mikealberto <alberto.michael1701@gmail.com>
2022-10-18 13:28:41 -0400

    seed data into mongodb

--- seed.js
+++ seed.js
@@ -1,4 +1,25 @@
 require("dotenv").config();
 require("./config/database");
 
+//model required to perform CRUD
+const Bread = require("./models/bread");
 const data = require("./data");
+
+
+//seeding database with async-await
+async function seedAsync() {
+    try {
+        //delete to avoid duplicate data
+        console.log("deleting data....");
+        await Bread.deleteMany({});
+        console.log("seeding data....");
+        await Bread.create(data.breads);
+        process.exit();
+
+    } catch (err) {
+        console.log(err)
+        process.exit();
+    }
+}
+
+seedAsync();
