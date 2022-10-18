58169120963794d0f16e6906c9cdfab02af6bccb
mikealberto <alberto.michael1701@gmail.com>
2022-10-18 15:19:35 -0400

    mount reviewsRouter and add middleware to get us the proper starter route for reviews

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ require("./config/passport");
 
 var indexRouter = require('./routes/index');
 const breadsRouter = require("./routes/breads");
+const reviewsRouter = require("./routes/reviews");
 
 
 var app = express();
@@ -47,6 +48,7 @@ app.use(function(req, res, next) {
 
 app.use('/', indexRouter);
 app.use("/breads", breadsRouter);
+app.use("/", reviewsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
