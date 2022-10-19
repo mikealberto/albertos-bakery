d9b71a84fd9338820f57cd95a2e4f3dc88605e7c
mikealberto <alberto.michael1701@gmail.com>
2022-10-19 11:17:41 -0400

    remove edit route from breads and add it to the reviews routes

--- routes/reviews.js
+++ routes/reviews.js
@@ -5,7 +5,10 @@ const isLoggedIn = require("../config/auth");
 
 //isLoggedIn for that extra layer of security
 router.post("/breads/:id/reviews", isLoggedIn, reviewsCtrl.create);
+//delete functionality
 router.delete("/reviews/:id", isLoggedIn, reviewsCtrl.delete);
+//edit functionality
+router.get("/:id/edit", isLoggedIn, reviewsCtrl.edit);
 
 module.exports = router;
 
